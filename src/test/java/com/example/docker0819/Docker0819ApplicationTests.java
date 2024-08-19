c5269f88ed65e403f6d7da20d355f53e1317bc44
Your Name <somethingisnothing125@gmail.com>
2024-08-19 16:02:25 +0900

    Dockerfile upload

---
+++ src/test/java/com/example/docker0819/Docker0819ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker0819;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker0819ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
